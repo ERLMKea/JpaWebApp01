d67ad819203a04ba2bab97e247001ff44bda25dc
ERLMKea <erlm@kea.dk>
2022-09-12 08:59:20 +0200

    Initial commit

---
+++ src/main/java/com/example/jpawebapp01/JpaWebApp01Application.java
@@ -0,0 +1,13 @@
+package com.example.jpawebapp01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaWebApp01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaWebApp01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jpawebapp01/JpaWebApp01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpawebapp01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaWebApp01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
